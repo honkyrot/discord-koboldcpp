f6a0b5efa8a6e6c40fde8eb0e9ee71b2a060e24e
honkyrot <111529160+honkyrot@users.noreply.github.com>
2024-10-02 01:40:22 -0400

    v2 commit
    updated

--- README.md
+++ README.md
@@ -1,13 +1,22 @@
 # discord-koboldcpp
-An attempt to use the **[koboldcpp](https://github.com/LostRuins/koboldcpp)** API with a discord chat bot.
+An attempt to use the **[koboldcpp](https://github.com/LostRuins/koboldcpp)** API with a simple discord chatbot.<br>
+This is very much a work in progress, and made for fun.<br>
+responds only to the bot name you give it or @\<bot\>.<br>
 
 ## Usage
 Clone or copy the repository.<br>
-Rename the `.env.example` to `.env` and change it's parameters.<br>
-On the target computer, run koboldcpp and the main.py file.<br>
-
+Rename the `.env.example` to `.env` and change its parameters.<br>
+On the target computer, run koboldcpp and THEN the main.py file.<br>
+9
 ## Note
-Your mileage may vary dependnig on your Large Language Model, instruct prompts, and samples, please adjust them accordingly to your liking.<br>
+Your mileage may vary depending on your Large Language Model, instruct prompts, and samples, please adjust them accordingly to your liking.<br>
+Personally, I don't recommend models tuned on roleplay if you want a general chatbot, but you do you.<br>
+Best used if you have knowledge on Python, AI LLMs, instruct mode, and koboldcpp.<br>
+
+## Features
+customizable<br>
+memory / chat history remembrance <br>
+commands (kinda)<br>
 
 #
 Uses Python 3.12.2
--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # Created by Honkyrot on 9/23/2024
 # https://github.com/honkyrot
 # Python 3.12.4
+# v 0.2
 
 # token in .env
 
@@ -17,23 +18,31 @@ from dotenv import load_dotenv
 load_dotenv()
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 DISCORD_GUILD = os.getenv('DISCORD_GUILD')
+LOCALHOST_ENDPOINT = os.getenv('LOCALHOST_ENDPOINT')
 
-ALLOW_ALL_GUILDS = True # default false, set to true if you want to allow all guilds
-# stats
-
+# bot should only be in one guild
+ALLOW_ALL_GUILDS = True # default false, set to true if you want to allow all guilds, not recommended since memory is persistent
 is_in_right_guild = False
 
 # ai stuff
 bot_name = "Cirno" # the name of the bot that will respond to
-chat_history = []  # store chat history
+# make sure to change the instruct prompt to fit in character
+always_respond_to = [] # list of names to always respond to whenever they send a message
 
-snip_words = ["<|im_end|>", "<|"]
+busy = False # prevent multiple requests
+
+allow_chat_history = True  # turn on or off chat history
+chat_history = []  # store chat history
+previous_prompt = ""
+
+# stop words for koboldcpp prompt
+snip_words = ["User:", "Bot:", "You:", "Me:", "{}:".format(bot_name)]
 # serialize snip_words into json
 snip_words = json.dumps(snip_words)
 
 # AI samples and parameters
-max_context_length = 2048 # the maximum number of tokens to hold in memory
-max_length = 512
+max_context_length = 4096 # the maximum number of tokens to hold in memory
+max_length = 256 # the maximum number of tokens to generate
 quiet = 'false'
 repetition_penalty = 1.1
 rep_pen_range = 256
@@ -44,7 +53,7 @@ top_a = 0
 top_k = 100
 top_p = 0.9
 typical = 1
-
+# dynamic temperature
 dynamic_temperature = 'true'
 dynatemp_low = 0.1
 dynatemp_high = 1.5
@@ -55,12 +64,19 @@ dynatemp_exponent = 1
 intents = discord.Intents.all()
 client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
+guild = None
 
-async def check_koboldcpp_status():
+async def sanitize_text(text):
+    """removes unwanted characters from the text"""
+    text = text.replace('"', '\\"')
+    text = text.replace('\n', '\\n')
+    text = text.replace('\\\\', '\\')
+    return text
+
+async def check_api_status():
     """checks if the koboldcpp api is online, returns True if it is
     and False if it is not"""
-    localhost = 'http://localhost:5001'
-    test_url = 'http://localhost:5001/api/v1/model'
+    test_url = f'{LOCALHOST_ENDPOINT}/api/v1/model'
 
     # make a GET request to the server
     response = requests.get(test_url)
@@ -71,12 +87,12 @@ async def check_koboldcpp_status():
     
 async def send_prompt(user, prompt):
     """Sends a pre-made prompt to the koboldcpp api."""
-    post_url = 'http://localhost:5001/api/v1/generate'
+    post_url = f'{LOCALHOST_ENDPOINT}/api/v1/generate'
 
     # build the prompt. VERY IMPORTANT TO USE THE RIGHT INSTRUCT PROMPTS TO THE MODEL, and help me so please.
     # just copy it from SillyTavern...
     # MODEL NAME USED IN DEVELOPMENT: https://huggingface.co/Sao10K/L3-8B-Stheno-v3.2 Q8.0 quant
-    instruct_prompt = "<|begin_of_text|><|start_header_id|>system<|end_header_id|>You are " + bot_name + ", a chatbot designed to interact with users. Respond in English, respond with only one sentence. Respond in plaintext. Keep your response in character, write a response that appropriately completes the request."
+    instruct_prompt = "<|begin_of_text|><|start_header_id|>system<|end_header_id|>You are " + bot_name + ", a character from the Touhou Project. You are a chatbot designed to interact with Discord users. Respond in English. Respond with only one sentence, keep it short. Respond in plaintext. Keep your response in character. Write a response that appropriately completes the request."
 
     for interaction in chat_history:
         print(interaction)
@@ -89,6 +105,10 @@ async def send_prompt(user, prompt):
     # remove that "f" from the prompt
     prompt = prompt.replace('"prompt": f"', '"prompt": "')
     print(prompt)
+
+    # store old prompt
+    global previous_prompt
+    previous_prompt = prompt
     
     # pre_prompt="""{"max_context_length": 8196,"max_length": 1024,"prompt": "%s %s","quiet": false,"rep_pen": 1.1,"rep_pen_range": 256,"rep_pen_slope": 1,"temperature": 0.5,"tfs": 1,"top_a": 0,"top_k": 100,"top_p": 0.9,"typical": 1}"""
 
@@ -110,73 +130,130 @@ async def on_ready() -> None:
     if guild is None:
         raise RuntimeError(f'Bot is not in the guild: {DISCORD_GUILD}')
 
-    # check the koboldcpp api
-    if not await check_koboldcpp_status():
-        raise RuntimeError('Koboldcpp api is not online!')
+    # check api status
+    if not await check_api_status():
+        raise RuntimeError('koboldcpp api is not online!')
     
     print(f'{client.user} has connected to Discord!')
-
-
+    
 @client.event
-async def on_message(message):
-    """on message, respond with a hello world""" 
+async def on_message(message: discord.Message) -> None:
+    """Responds to messages with koboldcpp."""
+    do_not_save_override = False
 
-    # don't respond to ourselves
+    # Ignore messages from ourselves
     if message.author == client.user:
         return
-    
-    # vars
-    user = message.author
-    prompt = message.content
-    guild_check = message.guild.name
 
-    # check if the bot is in the right guild
-    if not ALLOW_ALL_GUILDS and guild_check != DISCORD_GUILD:
-        print("Not in right guild, skipping message, got: " + prompt)
+    # Check if the bot is in the right guild
+    if not ALLOW_ALL_GUILDS and message.guild.name != DISCORD_GUILD:
         return
 
-    # make sure the bot was mentioned to continue
-    if not bot_name.lower() in prompt.lower():
-        print(f"{bot_name} was not mentioned, skipping message, got: {prompt}")
+    # Check if the bot was mentioned or if the bot name is in the message
+    if client.user.mentioned_in(message) or bot_name.lower() in message.content.lower():
+        # Remove the client ID when mentioned by an @
+        message.content = message.content.replace(f'<@{client.user.id}>', '')
+    else:
+        # Check if the user matches the always_respond_to list
+        if message.author.name.lower() in (name.lower() for name in always_respond_to):
+            pass
+        else:
+            return
+
+    # If the message is empty, don't respond
+    if message.content.strip() == '':
         return
     
-    # remove the backslash
-    prompt = prompt.replace('\\', '')
+    # convert any @ mentions to usernames
+
+    guild_members = message.guild.members
+    for list_member in guild_members:
+        message.content = message.content.replace(f'<@{list_member.id}>', list_member.name)
     
-    print('Message from {0.author}: {0.content}'.format(message))
+    # Check if the bot is busy
+    global busy
+    if busy:
+        return
+    busy = True
+    channel = message.channel
+    async with channel.typing(): # typing indicator
+        # Change some characters to work with the prompt
+        prompt = await sanitize_text(message.content)
+        # print(f'Prompt: {prompt}')
 
-    # send to AI for response
-    response, stored_response = await send_prompt(user, prompt)
+        # Send to AI for response
+        response, _ = await send_prompt(message.author, prompt)
 
-    decoded_response = json.loads(response)
-    try:
-        decoded_response = decoded_response['results'][0]['text']
-    except:
-        await message.channel.send('Error: ' + response)
-        raise RuntimeError('Error: ' + response)
-    # send response
-    
-    # dummy response
-    #await message.channel.send('Hello!')
+        # Decode response
+        try:
+            response = json.loads(response)['results'][0]['text']
+        except (KeyError, IndexError):
+            await message.channel.send(f'Error: {response}')
+            do_not_save_override = True
+            # raise RuntimeError(f'Error: {response}')
 
-    # actual response, need refining process
-    await message.channel.send(decoded_response)
+        # Send response
+        await message.channel.send(response)
 
-    # if everything works, add to history
-    chat_history.append({"user_name": user.name, "user_prompt": prompt, "bot_response": decoded_response})
+        busy = False
 
-@tree.command(
-    name="clear_history",
-    description="Clears the chat history of the bot.",
-)
+        # Add to history
+        if allow_chat_history and not do_not_save_override:
+            chat_history.append(
+                {'user_name': message.author.name, 'user_prompt': prompt, 'bot_response': await sanitize_text(response)}
+            )
+# commands below
 
-async def clear_history(interaction: discord.Interaction):
+@tree.command(name="clear_history", description="Clears the chat history of the bot.")
+async def clear_chat_history(interaction: discord.Interaction):
     """Clears the chat history of the bot."""
     global chat_history
+    num_interactions = len(chat_history)
     chat_history = []
-    await interaction.response.send_message("Chat history cleared.")
+    await interaction.response.send_message(
+        f"Chat history cleared.\n{num_interactions} interactions removed."
+    )
 
+@tree.command(name="show_history", description="Shows 6 most recent interactions.")
+async def show_chat_history(interaction: discord.Interaction):
+    """Shows 6 most recent interactions."""
+    global chat_history
+    global previous_prompt
 
-client.run(DISCORD_TOKEN)
+    temp_chat_history = []
+    for i in range(6):
+        temp_chat_history.append(chat_history[:].pop())
+
+    await interaction.response.send_message(f"Showing chat history stored:\n{temp_chat_history}")
+
+@tree.command(name="koboldcpp_api_model_name", description="Returns the current model name used by koboldcpp.")
+async def get_koboldcpp_api_model_name(interaction: discord.Interaction):
+    """Returns the current model name used by koboldcpp."""
+    url = f'{LOCALHOST_ENDPOINT}/api/v1/model'
+
+    # make a GET request to the server
+    response = requests.get(url)
+
+    # check if the server is up
+    if response.status_code == 200:
+        await interaction.response.send_message(f"The model name in use is:\n{response.text}")
+    else:
+        await interaction.response.send_message('Error: ' + response.text)
+
+@tree.command(name="pop_response", description="Pops/removes the last response from the chat history.")
+async def pop_response(interaction: discord.Interaction):
+    """Pops/removes the last response from the chat history."""
+    global chat_history
+    if len(chat_history) > 0:
+        pop = chat_history.pop()
+        await interaction.response.send_message(f"Chat history popped, removed: {pop}")
+    else:
+        await interaction.response.send_message("Chat history is empty.")
+
+try:
+    client.run(DISCORD_TOKEN)
+except discord.errors.LoginFailure:
+    print("bot failed to login.")
+
 
 # terminate with ctrl + c
